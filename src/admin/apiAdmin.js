1c0a9e90f30bab999c9a4aa43d31bc3073c95c19
JasmineKhalimova <jasmine.khalimova@yahoo.com>
2020-05-19 18:26:20 -0400

    product form complete

--- src/admin/apiAdmin.js
+++ src/admin/apiAdmin.js
@@ -1,6 +1,6 @@
 import { API } from '../config';
 
-// Category create method
+// Category Create Method
 export const createCategory = (userId, token, category) => {
     return fetch(`${API}/category/create/${userId}`, {
         method: 'POST',
@@ -17,4 +17,44 @@ export const createCategory = (userId, token, category) => {
         .catch(err => {
             console.log(err);
         });
+};
+
+// Create Product Method
+export const createProduct = (userId, token, product) => {
+    return fetch(`${API}/product/create/${userId}`, {
+        method: 'POST',
+        headers: {
+            Accept: 'application/json',
+            Authorization: `Bearer ${token}`
+        },
+        body: product
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => {
+            console.log(err);
+        });
+};
+
+// Get all the ategories method 
+export const getCategories = () => {
+    return fetch(`${API}/categories`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
+};
+
+// Get categories by id
+export const getCategory = categoryId => {
+    return fetch(`${API}/category/${categoryId}`, {
+        method: 'GET'
+    })
+        .then(response => {
+            return response.json();
+        })
+        .catch(err => console.log(err));
 };
